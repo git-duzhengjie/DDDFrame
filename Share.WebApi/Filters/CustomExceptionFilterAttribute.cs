6a8c16039a30ff3cdf4874ce92981d38d88bf5c5
政颉 杜 <zhengjie.du@foxmail.com>
2025-01-03 16:11:07 +0800

    x

--- Share.WebApi/Filters/CustomExceptionFilterAttribute.cs
+++ Share.WebApi/Filters/CustomExceptionFilterAttribute.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
