f84edc88fcc73202de2a4ad1a9215cc66391699e
政颉 杜 <zhengjie.du@foxmail.com>
2025-02-19 13:21:41 +0800

    x

---
+++ Infra.Core/Extensions/EnvironmentExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Core.Extensions
+{
+    public static class EnvironmentExtension
+    {
+        public static bool IsWin(this OperatingSystem os)
+        {
+            PlatformID platform = os.Platform;
+            switch (platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    return true;
+            }
+            return false;
+        }
+    }
+}
